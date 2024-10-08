603762d7094de0f6d4bc3c189e5324d9b8b21197
Charlie Bird <cbvvrd@gmail.com>
2024-10-08 14:34:31 +0100

    added js to show/hide img

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+
+document.querySelector("h1").addEventListener("click", onClick);
+let cheeseImg = document.querySelector("img");
+console.log(cheeseImg)
+
+function onClick() {
+    cheeseImg.classList.toggle("hidden");
+    console.log("clicked");
+}
