1e8a5e359b8066960586e4f9b16405ba064ac259
Charlie Bird <cbvvrd@gmail.com>
2024-10-08 16:28:37 +0100

    removed redundant console.log lines

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ console.log("hello world");
 
 document.querySelector("h1").addEventListener("click", onClick);
 let cheeseImg = document.querySelector("img");
-console.log(cheeseImg)
 
 function onClick() {
     cheeseImg.classList.toggle("hidden");
-    console.log("clicked");
 }
